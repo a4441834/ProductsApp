8fdb1af8a50c83e603530d6a1fece06713fb2dcb
Subbu Govindh <sub_mcse@yahoo.com>
2019-06-03 14:50:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 
 // Set up mongoose connection
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb://someuser:abcd1234@ds123619.mlab.com:23619/productstutorial';
+var dev_db_url = 'mongodb://mongouser:mongopassword@mongodb-34-centos7.msa-poc-dev.svc.cluster.local:27017/productstutorial';
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
 
-var port = 1234;
+var port = 9092;
 
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
